f6f8dc9c371a62ccd969d2602c2b1d9956e5f58e
Nathalia Rodrigues <nathalia.rsv@outlook.com>
2026-07-01 10:40:39 -0300

    feat: initial project structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from 'antd'
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Button type="primary">
+        Ant Design Funcionando
+      </Button>
+    </div>
+  )
+}
+
+export default App
